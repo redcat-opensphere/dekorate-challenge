a4fab014da31c5ab74cbcfe8011ab7e0ffc78331
user7 <david.gonzaloblanco@gruposantander.com>
2021-10-05 00:38:04 +0000

    Working with jib @ dockerhub

--- buildDeployJib.sh
+++ buildDeployJib.sh
@@ -1,6 +1,3 @@
-oc login --token=sha256~qyzY-3-eqI2-2YY72n11XzpejROte7MJDySI1IdxLWg --server=https://api.cluster-gbsmc.gbsmc.sandbox13.opentlc.com:6443
-#IMAGE=image-registry.openshift-image-registry.svc:5000/user7-challenge2/redcat-dekorate-challenge
-#mvn compile jib:build -Dimage=$IMAGE -Dmaven.wagon.http.ssl.insecure=true -Djib.to.auth.username=user7 -Djib.to.auth.password=openshift
-
-# https://github.com/GoogleContainerTools/jib/tree/master/examples/spring-boot
-mvn clean package -Ddekorate.build=true -DskipTests -Dmaven.wagon.http.ssl.insecure=true -Djib.to.auth.username=unused -Djib.to.auth.password=sha256~d4UFMMAFIRig8vPUP4BpkJ6Hkj9bHFR824e0mObXOV4
+oc login --token=sha256~sK6nbIf_akoRM2xooo4bX85Dy5DxnvT0eXqoTfP73is --server=https://api.cluster-gbsmc.gbsmc.sandbox13.opentlc.com:6443
+oc project user7-challenge2
+mvn clean package -DskipTests -Ddekorate.build=true
